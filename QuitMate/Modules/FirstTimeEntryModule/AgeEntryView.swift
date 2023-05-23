d480834e553aa7f265fd8f6b25231bba2d9c65e8
Oleksandr Vasylenko <sash.vas2001@gmail.com>
2023-05-24 02:45:38 +0300

    Added views for first time entry

---
+++ QuitMate/Modules/FirstTimeEntryModule/AgeEntryView.swift
@@ -0,0 +1,20 @@
+//
+//  AgeEntryView.swift
+//  QuitMate
+//
+//  Created by Саша Василенко on 24.05.2023.
+//
+
+import SwiftUI
+
+struct AgeEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AgeEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        AgeEntryView()
+    }
+}
